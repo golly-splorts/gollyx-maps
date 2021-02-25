665bfe23c2a99dda5d7ae028b957028998232555
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-02-25 15:35:57 -0800

    set minimum map size

--- golly_maps/maps.py
+++ golly_maps/maps.py
@@ -12,7 +12,7 @@ from .patterns import (
     pattern_union,
 )
 from .utils import pattern2url, retry_on_failure
-from .error import GollyPatternsError
+from .error import GollyPatternsError, GollyMapsError
 
 
 ##############
@@ -72,6 +72,8 @@ def get_map_realization(patternname, rows=100, columns=120):
         "cellSize:" k
     }
     """
+    if rows < 100 or columns < 120:
+        raise GollyMapsError(f"Error: you must have at least 100 rows and 120 columns")
     # Get map data (pattern, name, zone names)
     mapdat = get_map_metadata(patternname)
 
