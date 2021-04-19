fef576b88f77de964163c652f01a9ccc8d044ed9
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-04-19 13:22:44 -0700

    add methuselah grid and porch lights maps

--- gollyx_maps/toroidal.py
+++ gollyx_maps/toroidal.py
@@ -8,6 +8,7 @@ from .patterns import (
     get_pattern_size,
     get_pattern_livecount,
     get_grid_pattern,
+    get_grid_empty,
     segment_pattern,
     methuselah_quadrants_pattern,
     pattern_union,
@@ -100,7 +101,10 @@ def toroidal_methuselah_quadrants_pattern(
                     + random.randint(-jitterx, jitterx)
                 )
 
-                meth = random.choice(methuselah_names)
+                if fixed_methuselah:
+                    meth = fixed_methuselah
+                else:
+                    meth = random.choice(methuselah_names)
 
                 try:
                     pattern = get_grid_pattern(
@@ -271,7 +275,7 @@ def doublegaussian_twocolor(rows, cols, seed=None):
 
 def donutengine_twocolor(rows, cols, seed=None):
     team1_pattern, team2_pattern = toroidal_methuselah_quadrants_pattern(
-        rows, cols, seed=seed
+        rows, cols, seed=seed, fixed_methuselah="switchengine"
     )
     pattern1_url = pattern2url(team1_pattern)
     pattern2_url = pattern2url(team2_pattern)
@@ -677,7 +681,12 @@ def donutrandomsegment_twocolor(rows, cols, seed=None):
 
 
 def donutmethuselahs_twocolor(rows, cols, seed=None):
-    pass
+    team1_pattern, team2_pattern = toroidal_methuselah_quadrants_pattern(
+        rows, cols, seed=seed
+    )
+    pattern1_url = pattern2url(team1_pattern)
+    pattern2_url = pattern2url(team2_pattern)
+    return pattern1_url, pattern2_url
 
 
 def donutmath_twocolor(rows, cols, seed=None):
@@ -732,7 +741,84 @@ def randys_twocolor(rows, cols, seed=None):
 
 
 def porchlights_twocolor(rows, cols, seed=None):
-    pass
+    if seed is not None:
+        random.seed(seed)
+
+    nsegments = 2
+    thickness = random.randint(1, 3)
+
+    team1_pattern = get_grid_empty(rows, cols, flat=False)
+    team2_pattern = get_grid_empty(rows, cols, flat=False)
+
+    jitterx = 4
+    jittery = 4
+    intersectys = [(j+1)*rows//(nsegments+1) + random.randint(-jittery, jittery) for j in range(nsegments)]
+    random.shuffle(intersectys)
+
+    def _get_bounds(z, dim):
+        zstart = z - dim//2
+        zend = z + (dim - dim//2)
+        return zstart, zend
+
+    # Add the string
+    y1s = intersectys[:len(intersectys)//2]
+    y2s = intersectys[len(intersectys)//2:]
+    for ix in range(0, cols):
+
+        for y1 in y1s:
+            for iy in range(*_get_bounds(y1, thickness)):
+                team1_pattern[iy][ix] = 'o'
+
+        for y2 in y2s:
+            for iy in range(*_get_bounds(y2, thickness)):
+                team2_pattern[iy][ix] = 'o'
+
+    # Add some lights to the string
+
+    for y1 in y1s:
+        b = _get_bounds(y1, thickness)
+        maxy = max(b)
+        miny = min(b)
+        ylightstop = miny - random.randint(2, 3)
+        ylightsbot = maxy + random.randint(2, 3)
+        ix = random.randint(4, 12)
+        while ix < cols-1:
+            if random.random() < 0.50:
+                team1_pattern[ylightsbot][ix] = 'o'
+                team1_pattern[ylightsbot][ix+1] = 'o'
+                team1_pattern[ylightsbot+1][ix] = 'o'
+                team1_pattern[ylightsbot+1][ix+1] = 'o'
+            else:
+                team1_pattern[ylightstop][ix] = 'o'
+                team1_pattern[ylightstop][ix+1] = 'o'
+                team1_pattern[ylightstop-1][ix] = 'o'
+                team1_pattern[ylightstop-1][ix+1] = 'o'
+            ix += random.randint(10, 12) + random.randint(-jitterx, jitterx)
+
+    for y2 in y2s:
+        b = _get_bounds(y2, thickness)
+        maxy = max(b)
+        miny = min(b)
+        ylightstop = miny - random.randint(2, 3)
+        ylightsbot = maxy + random.randint(2, 3)
+        ix = random.randint(4, 12)
+        while ix < cols-1:
+            if random.random() < 0.50:
+                team2_pattern[ylightsbot][ix] = 'o'
+                team2_pattern[ylightsbot][ix+1] = 'o'
+                team2_pattern[ylightsbot+1][ix] = 'o'
+                team2_pattern[ylightsbot+1][ix+1] = 'o'
+            else:
+                team2_pattern[ylightstop][ix] = 'o'
+                team2_pattern[ylightstop][ix+1] = 'o'
+                team2_pattern[ylightstop-1][ix] = 'o'
+                team2_pattern[ylightstop-1][ix+1] = 'o'
+            ix += random.randint(10, 12) + random.randint(-jitterx, jitterx)
+
+    pattern1_url = pattern2url(team1_pattern)
+    pattern2_url = pattern2url(team2_pattern)
+
+    return pattern1_url, pattern2_url
 
 
 def crabdonuts_twocolor(rows, cols, seed=None):
