6f0037a88bcee6d8c365675432eb3753479a12d4
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-06-28 14:05:30 -0700

    add lake

--- examples/dragon/dragon.py
+++ examples/dragon/dragon.py
@@ -3,68 +3,27 @@ from gollyx_maps.patterns import get_grid_empty
 import random
 
 
-name = 'vector 2'
+name = "matrix"
 COLS = 200
+NPARTS = 8
+ALIVE_DENSITY = 0.5
 
 
-def make_map(seed=None):
-
+def make_map(name, seed=None):
     cols = COLS
 
     # set rng seed (optional)
     if seed is not None:
         random.seed(seed)
 
-    # This will return a line of '.' (dead cells), use 'o' to mark cells alive
-    # Use [0] to remove the (empty) outer dimension and just get the row of interest
-    team1_pattern = get_grid_empty(1, cols, flat=False)[0]
-    team2_pattern = get_grid_empty(1, cols, flat=False)[0]
-    patterns = [team1_pattern, team2_pattern]
+    fmap = {"starfield": starfield, "vector": vector, "matrix": matrix, "lake": lake}
 
-
-    # ---------
-    # Starfield
-    if name == 'starfield':
-
-        # Parameters:
-        min_stars = 3
-        max_stars = 9
-        nstars = random.randint(min_stars, max_stars)
-
-        for color in range(2):
-            for star in range(nstars):
-                randloc = random.randint(0, cols-1)
-                while (patterns[0][randloc]=='o' or patterns[1][randloc]=='o'):
-                    randloc = random.randint(0, cols-1)
-                patterns[color][randloc] = 'o'
-
-
-    # ------
-    # Vector
-    elif name.startswith('vector'):
-
-        # Parameters:
-        nparts = 2
-        alive_density = 0.20
-
-        half = nparts//2
-        alive_cells_each_color = (alive_density/2)*cols
-        alive_cells_each_partition = round(alive_cells_each_color/half)
-        partwidth = round(cols/nparts)
-        colors = [0, 1]
-        colorparts = [0,]*half + [1,]*half
-        random.shuffle(colorparts)
-
-        for i, part in enumerate(colorparts):
-            color = part
-            added = 0
-            for j in range(alive_cells_each_partition):
-                # Adjust for partition boundaries
-                randloc = random.randint(i*partwidth, min((i+1)*partwidth - 1, cols - 1))
-                while (patterns[0][randloc]=='o' or patterns[1][randloc]=='o'):
-                    randloc = random.randint(i*partwidth, min((i+1)*partwidth - 1, cols - 1))
-                patterns[color][randloc] = 'o'
-
+    if name not in fmap:
+        raise Exception(f"Could not find map name {name}")
+    else:
+        nparts = NPARTS
+        f = fmap[name]
+        patterns = f(cols, nparts)
 
     # Wrap in [] to re-insert the (empty) outer dimension
     s1 = pattern2url([patterns[0]])
@@ -74,5 +33,201 @@ def make_map(seed=None):
     print(url)
 
 
+def empty_dragon_patterns(cols):
+    """
+    Return a line of 1D patterns filled with '.' (dead cells).
+    Use 'o' to mark alive cells.
+    This strips out the empty outer dimension,
+    make sure to add it back in by surrounding the pattern
+    with [] before you do anything with the pattern.
+    """
+    # Use [0] to remove the (empty) outer dimension and just get the row of interest
+    team1_pattern = get_grid_empty(1, cols, flat=False)[0]
+    team2_pattern = get_grid_empty(1, cols, flat=False)[0]
+    patterns = [team1_pattern, team2_pattern]
+    return patterns
+
+
+def starfield(cols, nparts, seed=None):
+    """
+    Starfield: very sparse, one partition
+    """
+    # We don't use nparts, it is just there so we have consistent method signatures
+    if seed is not None:
+        random.seed(seed)
+
+    # Parameters:
+    min_stars = 3
+    max_stars = 9
+    nstars = random.randint(min_stars, max_stars)
+
+    patterns = empty_dragon_patterns(cols)
+
+    # For each color, for each star, turn on one cell
+    for color in range(2):
+        for star in range(nstars):
+            randloc = random.randint(0, cols - 1)
+            while patterns[0][randloc] == "o" or patterns[1][randloc] == "o":
+                randloc = random.randint(0, cols - 1)
+            patterns[color][randloc] = "o"
+
+    return patterns
+
+
+def vector(cols, nparts, seed=None):
+    """
+    Vector: one-color random split
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    assert nparts > 0
+
+    if nparts%2 == 1:
+        nparts += 1
+
+    # Parameters:
+    alive_density = ALIVE_DENSITY
+
+    patterns = empty_dragon_patterns(cols)
+
+    # We require minimum of 2 partitions
+    if nparts < 2:
+        nparts = 2
+
+    if nparts%2 == 0:
+
+        # If even number of partitions,
+        # split them 50/50 between color1/color2
+        half = nparts // 2
+        alive_cells_each_color = (alive_density / 2) * cols
+        alive_cells_each_partition = round(alive_cells_each_color / half)
+        partwidth = round(cols / nparts)
+        colorparts = [0,] * half + [
+            1,
+        ] * half
+        random.shuffle(colorparts)
+
+    for i, color in enumerate(colorparts):
+
+        for j in range(alive_cells_each_partition):
+            # Adjust for partition boundaries
+            randloc = random.randint(
+                i * partwidth, min((i + 1) * partwidth - 1, cols - 1)
+            )
+            while patterns[0][randloc] == "o" or patterns[1][randloc] == "o":
+                randloc = random.randint(
+                    i * partwidth, min((i + 1) * partwidth - 1, cols - 1)
+                )
+            patterns[color][randloc] = "o"
+
+    return patterns
+
+
+def matrix(cols, nparts, seed=None):
+    """
+    Matrix: two-color random split
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    # Parameters:
+    alive_density = ALIVE_DENSITY
+
+    # total, not per color
+    alive_cells_each_partition = round((alive_density * cols) / nparts)
+    partwidth = round(cols / nparts)
+
+    patterns = empty_dragon_patterns(cols)
+
+    if nparts % 2 == 0:
+
+        # If even number of partitions,
+        # leave one totally dead, and the rest
+        # two-color random split
+        alivedead = [0] + [
+            1,
+        ] * (nparts - 1)
+        random.shuffle(alivedead)
+
+    else:
+
+        # If odd number of partitions,
+        # every other partition is dead
+        alivedead = []
+        for k in range(nparts):
+            alivedead.append((k + 1) % 2)
+
+    for i, ald in enumerate(alivedead):
+        if ald != 0:
+            for j in range(alive_cells_each_partition):
+                randloc = random.randint(
+                    i * partwidth, min((i + 1) * partwidth - 1, cols - 1)
+                )
+                while patterns[0][randloc] == "o" or patterns[1][randloc] == "o":
+                    randloc = random.randint(
+                        i * partwidth, min((i + 1) * partwidth - 1, cols - 1)
+                    )
+                # Split 50/50 between colors
+                color = j % 2
+                patterns[color][randloc] = "o"
+
+    return patterns
+
+
+def lake(cols, nparts, seed=None):
+    """
+    Lake: one-color streak
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    assert nparts > 0
+
+    if nparts%2 == 1:
+        nparts += 1
+
+    # Parameters:
+    mean_size = 5
+
+    patterns = empty_dragon_patterns(cols)
+
+    half = nparts // 2
+    partwidth = round(cols / nparts)
+    colorparts = [0,] * half + [
+        1,
+    ] * half
+    random.shuffle(colorparts)
+
+    for i, color in enumerate(colorparts):
+        pstart = i * partwidth
+        # end of partition (exclusive of the end)
+        pend = min((i + 1) * partwidth, cols)
+        ip = pstart
+        alivedeadsymbol = "."
+        while ip < pend:
+
+            # Flip the switch
+            if alivedeadsymbol == ".":
+                alivedeadsymbol = "o"
+            elif alivedeadsymbol == "o":
+                alivedeadsymbol = "."
+
+            # Generate a new interval length
+            interval = round(random.expovariate(1.0 / mean_size))
+            if interval == 0:
+                interval = 1
+
+            # Loop over the interval, incrementing ip
+            # and making cells alive as we go.
+            for j in range(interval):
+                patterns[color][ip] = alivedeadsymbol
+                ip += 1
+                if ip >= pend:
+                    break
+
+    return patterns
+
+
 if __name__ == "__main__":
-    make_map()
+    make_map("lake")
