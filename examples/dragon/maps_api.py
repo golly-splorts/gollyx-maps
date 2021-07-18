ac499029156fc65fce03217e5ad5d7d9a5531dd9
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-07-18 12:47:47 -0700

    fix dragon maps api example

--- examples/dragon/maps_api.py
+++ examples/dragon/maps_api.py
@@ -16,10 +16,14 @@ patternNames = [
     "towers",
 ]
 
+patternNames = ["towers"]
+
 for patternName in patternNames:
     m = maps.get_map_realization("dragon", patternName, rows=rows, columns=cols)
     print("")
     print("Map: " + m['mapName'])
+    print("State 1:", m["initialConditions1"])
+    print("State 2:", m["initialConditions2"])
     print(
         "http://192.168.30.20:8888/simulator/index.html"
         + m["url"]
