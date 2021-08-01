743f5f0e9adcd83c335e7151f69a471c5e27b5e9
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-08-01 16:26:48 -0700

    expand on math expressions

--- gollyx_maps/rainbow.py
+++ gollyx_maps/rainbow.py
@@ -26,6 +26,7 @@ from .error import GollyXPatternsError, GollyXMapsError
 def get_rainbow_pattern_function_map():
     return {
         "random": random_fourcolor,
+        "rainbowmath": rainbowmath_fourcolor,
         "randommethuselahs": randommethuselahs_fourcolor,
         "orchard": orchard_fourcolor,
         "justyna": justyna_fourcolor,
@@ -35,6 +36,10 @@ def get_rainbow_pattern_function_map():
         "patiolights": patiolights_fourcolor,
         "rainbow": rainbow_fourcolor,
         "sunburst": sunburst_fourcolor,
+        "timebomb": timebomb_fourcolor,
+        "timebombredux": timebomb2_fourcolor,
+        "crabs": crabs_fourcolor,
+        "doublegaussian": doublegaussian_fourcolor,
     }
 
 
@@ -671,6 +676,7 @@ def _eightr_fourcolor(rows, cols, seed=None):
     return urls
 
 
+@retry_on_failure
 def patiolights_fourcolor(rows, cols, seed=None):
     """
     Patio lights pattern is a line segments with boxes placed randomly along the segment, like a string of lights
@@ -746,10 +752,12 @@ def patiolights_fourcolor(rows, cols, seed=None):
     return tuple(urls)
 
 
+@retry_on_failure
 def rainbow_fourcolor(rows, cols, seed=None):
     return _rainburst_fourcolor(rows, cols, seed, sunburst=False)
 
 
+@retry_on_failure
 def sunburst_fourcolor(rows, cols, seed=None):
     return _rainburst_fourcolor(rows, cols, seed, sunburst=True)
 
@@ -838,3 +846,377 @@ def _rainburst_fourcolor(rows, cols, seed=None, sunburst=False):
     random.shuffle(urls)
 
     return tuple(urls)
+
+
+@retry_on_failure
+def timebomb_fourcolor(rows, cols, seed=None):
+    return _timebomb_fourcolor(rows, cols, revenge=False, seed=seed)
+
+
+@retry_on_failure
+def timebomb2_fourcolor(rows, cols, seed=None):
+    return _timebomb_fourcolor(rows, cols, revenge=True, seed=seed)
+
+
+def _timebomb_fourcolor(rows, cols, revenge, seed=None):
+
+    if seed is not None:
+        random.seed(seed)
+
+    mindim = min(rows, cols)
+
+    # Geometry
+    # L = length scale
+    L = 20
+    centerx = cols // 2
+    centery = rows // 2
+
+    # Each team gets one oscillator and one timebomb
+    nteams = 4
+    team_assignments = list(range(nteams))
+    random.shuffle(team_assignments)
+
+    def _get_oscillator_name():
+        if revenge:
+            oscillators = ["airforce", "koksgalaxy", "dinnertable", "vring64", "harbor"]
+            which_oscillator = random.choice(oscillators)
+        else:
+            which_oscillator = "quadrupleburloaferimeter"
+        return which_oscillator
+
+    rotdegs = [0, 90, 180, 270]
+
+
+    urls = [None, None, None, None]
+
+    for iteam in range(nteams):
+        # Location:
+        # x = center + a*L
+        # y = center + b*L
+        # QI:   a = 1, b = 1
+        # QII:  a = -1, b = 1
+        # QIII: a = -1, b = -1
+        # QIV:  a = 1, b = -1
+        if iteam==0 or iteam==3:
+            a = 1
+        else:
+            a = -1
+
+        if iteam==0 or iteam==1:
+            b = 1
+        else:
+            b = -1
+
+        osc_x = centerx + a*L
+        osc_y = centery + b*L
+
+        bomb_x = centerx + 2*a*L
+        bomb_y = centery + 2*b*L
+
+        # jitter for patterns
+        osc_jitter_x = 3
+        osc_jitter_y = 3
+        timebomb_jitter_x = 6
+        timebomb_jitter_y = 6
+
+        osc_pattern = get_grid_pattern(
+            _get_oscillator_name(),
+            rows,
+            cols,
+            xoffset=osc_x + random.randint(-osc_jitter_x, osc_jitter_x),
+            yoffset=osc_y + random.randint(-osc_jitter_y, osc_jitter_y),
+            rotdeg=random.choice(rotdegs),
+        )
+
+        bomb_pattern = get_grid_pattern(
+            "timebomb",
+            rows,
+            cols,
+            xoffset=bomb_x + random.randint(-timebomb_jitter_x, timebomb_jitter_x),
+            yoffset=bomb_y + random.randint(-timebomb_jitter_y, timebomb_jitter_y),
+            rotdeg=random.choice(rotdegs),
+        )
+
+        team_pattern = pattern_union([osc_pattern, bomb_pattern])
+        team_url = pattern2url(team_pattern)
+
+        team_ix = team_assignments[iteam]
+        urls[team_ix] = team_url
+
+    return tuple(urls)
+
+
+def crabs_fourcolor(rows, cols, seed=None):
+
+    if seed is not None:
+        random.seed(seed)
+
+    rotdegs = [0, 90, 180, 270]
+
+    jitter = 1
+
+    # 8 crabs total
+    centerys = [rows//4, 3*rows//4]
+    centerxs = [cols//5, 2*cols//5, 3*cols//5, 4*cols//5]
+
+    nteams = 4
+    team_assignments = list(range(nteams))
+    random.shuffle(team_assignments)
+
+    crab_patterns = [[], [], [], []]
+
+    for i, (centerx, centery) in enumerate(itertools.product(centerxs, centerys)):
+        imod4 = i%4
+
+        crabcenterx = centerx + random.randint(-jitter, jitter)
+        crabcentery = centery + random.randint(-jitter, jitter)
+
+        crab = get_grid_pattern(
+            "crabstretcher",
+            rows,
+            cols,
+            xoffset=crabcenterx,
+            yoffset=crabcentery,
+            hflip=(random.random() < 0.5),
+            vflip=(random.random() < 0.5),
+            rotdeg=random.choice(rotdegs),
+        )
+        team_ix = team_assignments[imod4]
+        team_pattern = crab_patterns[team_ix]
+        team_pattern.append(crab)
+        crab_patterns[team_ix] = team_pattern
+
+    pattern_unions = [pattern_union(pl) for pl in crab_patterns]
+    urls = [pattern2url(pu) for pu in pattern_unions]
+    return tuple(urls)
+
+def doublegaussian_fourcolor(rows, cols, seed=None):
+
+    if seed is not None:
+        random.seed(seed)
+
+    # Lower bound of 0.10, upper bound of 0.15
+    density = 0.10 + random.random() * 0.05
+
+    ncells = rows * cols
+    nlivecells = ((ncells * density)//4)*4
+    nlivecellspt = nlivecells // 4
+
+    # Variable blobbiness
+    stdx = cols// random.randint(8, 16)
+    stdy = rows// random.randint(8, 16)
+
+    jitter = 5
+
+    nteams = 4
+    team_assignments = list(range(nteams))
+    random.shuffle(team_assignments)
+
+    centerxs = [cols//4, 3*cols//4] 
+    centerys = [rows//4, 3*rows//4]
+
+    urls = [None, None, None, None]
+
+    master_points = set()
+    for i, (centerx, centery) in enumerate(itertools.product(centerxs, centerys)):
+
+        team_ix = team_assignments[i]
+
+        cx = centerx + random.randint(-jitter, jitter)
+        cy = centery + random.randint(-jitter, jitter)
+
+        team_points = set()
+        while len(team_points) < nlivecellspt:
+            randx = int(random.gauss(cx, stdx))
+            randy = int(random.gauss(cy, stdy))
+            if (randx >= 0 and randx < cols) and (randy >= 0 and randy < rows):
+                if (randx, randy) not in master_points:
+                    team_points.add((randx, randy))
+                    master_points.add((randx, randy))
+
+        # Assemble the circle dot diagram for team
+        team_pattern = []
+        for y in range(rows):
+            this_row = []
+            for x in range(cols):
+                if (x, y) in team_points:
+                    this_row.append("o")
+                else:
+                    this_row.append(".")
+            this_rowstr = "".join(this_row)
+            team_pattern.append(this_rowstr)
+
+        team_url = pattern2url(team_pattern)
+        urls[team_ix] = team_url
+
+    return tuple(urls)
+
+
+#@retry_on_failure
+def rainbowmath_fourcolor(rows, cols, seed=None):
+
+    if seed is not None:
+        random.seed(seed)
+
+    def is_prime(n):
+        n = abs(n)
+        if n == 2 or n == 3: return True
+        if n < 2 or n%2 == 0: return False
+        if n < 9: return True
+        if n%3 == 0: return False
+        r = int(n**0.5)
+        # since all primes > 3 are of the form 6n ± 1
+        # start with f=5 (which is prime)
+        # and test f, f+2 for being prime
+        # then loop by 6.
+        f = 5
+        while f <= r:
+            if n % f == 0: return False
+            if n % (f+2) == 0: return False
+            f += 6
+        return True
+
+    def is_not_prime(n):
+        return not is_prime(n)
+
+    # Random choice of which form to use
+    #coin = random.randint(1,3)
+    coin = 8
+
+    if coin == 1:
+        p = random.choice([k*k for k in [5, 7, 9, 11]])
+        f = lambda x, y: int(is_not_prime((x*x & y*y) % p))
+
+    elif coin == 2:
+
+        # Linked diagonals of boxes
+        ab = [3, 4, 5]
+        a = random.choice(ab)
+        b = random.choice(ab)
+
+        cs = [16, 18, 20, 22]
+        c = random.choice(cs)
+
+        p = 7
+
+        f = lambda x, y: int((x//a ^ y//a)*c % p)
+
+    elif coin == 3:
+
+        # Linked diagonals of very large boxes
+        ab = [9, 10, 11]
+        a = random.choice(ab)
+        b = random.choice(ab)
+
+        cs = [16, 18, 20, 22]
+        c = random.choice(cs)
+
+        p = 7
+
+        f = lambda x, y: int((x//a ^ y//a)*c % p)
+
+
+    elif coin == 4:
+
+        # Sterpinsky triangles
+        ps = [7, 11, 13, 15, 35, 37]
+        p = random.choice(ps)
+        f = lambda x, y: int((x & y) % p)
+
+    elif coin == 5:
+
+        # This is a one-off that's in perfect sync and makes wild patterns
+        a = 3
+        b = 3
+        p = 99
+        f = lambda x, y: int((a**x)%p & (b**y)%p)
+
+    elif coin == 6:
+
+        a = random.randint(1,10)
+        b = random.randint(1,10)
+        p = 99
+
+        f = lambda x, y: int(is_not_prime((a*x & b*y) % p))
+
+
+    elif coin == 7:
+
+        ps = [81, 83, 85, 87, 89, 91, 93, 95, 97, 99]
+
+        p = random.choice(ps)
+
+        f = lambda x, y: int(is_not_prime((x//(y+1) ^ y) % p))
+
+
+    elif coin == 8:
+
+        ps = [69, 99, 299, 699, 999]
+
+        f = lambda x, y: int(is_not_prime((x*x//(y+1)) % p))
+
+
+    xoffset = 0
+    yoffset = 0
+
+    team_patterns = _expression_pattern(
+        rows,
+        cols,
+        seed,
+        f,
+        xoffset=xoffset,
+        yoffset=yoffset,
+    )
+
+    urls = [pattern2url(pat) for pat in team_patterns]
+
+    for url in urls:
+        if url == "[]":
+            raise GollyXPatternsError("Error with bitfield: everything is empty")
+
+    return tuple(urls)
+
+
+def _expression_pattern(
+    rows,
+    cols,
+    seed,
+    f_handle,
+    xoffset=0,
+    yoffset=0,
+):
+    nteams = 4
+
+    # These store the the .o diagrams (flat=False means these are lists of lists of one char)
+    team_patterns = []
+    for i in range(nteams):
+        tp = get_grid_empty(rows,cols,flat=False)
+        team_patterns.append(tp)
+
+    # Assemble a list of cells that are alive at the roots of f (if f returns 0)
+    coordinates = []
+    for xtrue in range(0, cols):
+        for ytrue in range(0, rows):
+            xtransform = xtrue - xoffset
+            ytransform = ytrue - yoffset
+            if f_handle(xtransform, ytransform) == 0:
+                coordinates.append((xtrue, ytrue))
+
+    # Shuffle live cell cordinates
+    random.shuffle(coordinates)
+
+    # Assign live cell coordinates to teams using serpentine pattern
+    team_order = list(range(nteams))
+    random.shuffle(team_order)
+    serpentine_pattern = list(team_order) + list(reversed(team_order))
+
+    for i, (x, y) in enumerate(coordinates):
+        serp_ix = i % len(serpentine_pattern)
+        team_ix = serpentine_pattern[serp_ix]
+        team_patterns[team_ix][y][x] = "o"
+
+    return team_patterns
+
+
+
+
