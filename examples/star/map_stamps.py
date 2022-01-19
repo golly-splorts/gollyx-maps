488e7d6de2a2c40401d2d01af92010badaa7a6bb
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2022-01-18 23:09:41 -0800

    add example stamps maps

--- examples/star/map_stamps.py
+++ examples/star/map_stamps.py
@@ -4,7 +4,9 @@ rows = 160
 cols = 240
 
 #m = maps.get_map_realization("star", "squarestar", rows=rows, columns=cols)
-m = maps.get_map_realization("star", "kitchensink", rows=rows, columns=cols)
+#m = maps.get_map_realization("star", "kitchensink", rows=rows, columns=cols)
+#m = maps.get_map_realization("star", "ricepudding", rows=rows, columns=cols)
+m = maps.get_map_realization("star", "fishsoup", rows=rows, columns=cols)
 
 print(
     "http://192.168.30.20:8888/index.html"
