a9589fc9497a174207220f9fd5c329a733b5872b
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-08-03 10:58:55 -0700

    hellmouth and ii all maps script

--- examples/hellmouth_maps.py
+++ examples/hellmouth_maps.py
@@ -7,4 +7,4 @@ print("")
 for pattern in maps.get_all_map_patterns('hellmouth'):
     print(f"{pattern}:")
     m = maps.get_map_realization('hellmouth', pattern)
-    print(f"http://192.168.30.20:8888/simulator/index.html{m['url']}")
+    print(f"https://v.golly.life/simulator/index.html{m['url']}")
