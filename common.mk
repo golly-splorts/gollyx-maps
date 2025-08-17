92778ef992417b96a87ff7f79bc847839eda83ff
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-08-17 10:27:09 -0700

    remove cruft

--- common.mk
+++
@@ -1,6 +0,0 @@
-SHELL=/bin/bash
-
-ifeq ($(shell echo ${GOLLYX_MAPS_HOME}),)
-$(error Environment variable GOLLYX_MAPS_HOME not defined. Please run "source environment" in the gollyx-maps repo root directory before running make commands)
-endif
-
