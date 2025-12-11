7e1c93e379e1ac0cdb98e04229d778db962f87f3
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-12-10 19:35:51 -0800

    add star maps stamps example

--- examples/star/map_stamps.py
+++ examples/star/map_stamps.py
@@ -1,21 +1,38 @@
 from gollyx_maps import maps
+from gollyx_maps.patterns import get_pattern_size, get_grid_pattern, pattern_union
+from gollyx_maps.utils import pattern2url
 
 rows = 160
 cols = 240
 
-def main():
+def maps():
     #m = maps.get_map_realization("star", "squarestar", rows=rows, columns=cols)
     m = maps.get_map_realization("star", "kitchensink", rows=rows, columns=cols)
     #m = maps.get_map_realization("star", "ricepudding", rows=rows, columns=cols)
     #m = maps.get_map_realization("star", "fishsoup", rows=rows, columns=cols)
     
     print(
-        "http://192.168.30.20:8888/index.html"
-        + m["url"]
-        + f"&rows={rows}&cols={cols}&cellSize=3"
+        "?" + m["url"] + f"&rows={rows}&cols={cols}&cellSize=3"
     )
 
+
+def stamps():
+
+    centerx1 = cols//2 + cols//4
+    centery1 = rows//2
+
+    centerx2 = cols//4
+    centery2 = rows//2
+
+    pattern1 = get_grid_pattern('scaffoldunfusing', rows, cols, xoffset=centerx1+5, yoffset=centery1+5)
+    pattern2 = get_grid_pattern('scaffoldunfusing', rows, cols, xoffset=centerx2,   yoffset=centery2, hflip=True)
+
+    s1 = pattern2url(pattern1)
+    s2 = pattern2url(pattern2)
+
+    url = f"?s1={s1}&s2={s2}"
+    print(url)
+
+
 if __name__=="__main__":
-    #import cProfile
-    #cProfile.run('main()')
-    main()
+    stamps()
