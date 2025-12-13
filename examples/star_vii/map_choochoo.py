bb4dfd9be9e2fbdff060085eb03deb53964f6718
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-12-13 14:36:22 -0800

    update choochoo map

--- examples/star_vii/map_choochoo.py
+++ examples/star_vii/map_choochoo.py
@@ -3,7 +3,10 @@ from gollyx_maps import maps
 rows = 180
 cols = 280
 
-m = maps.get_map_realization("star_vii", "choochoo", rows=rows, columns=cols)
+#m = maps.get_map_realization("star_vii", "twochoochoo", rows=rows, columns=cols)
+#m = maps.get_map_realization("star_vii", "midnightexpress", rows=rows, columns=cols)
+m = maps.get_map_realization("star_vii", "spaceelevator", rows=rows, columns=cols)
+#m = maps.get_map_realization("star_vii", "faradaycage", rows=rows, columns=cols)
 
 print(
     "http://localhost:8000/simulator/index.html"
