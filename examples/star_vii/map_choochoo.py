2d3a4127ff8b9564408a5b6b3ae4744faee23b1c
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-12-25 11:36:27 -0800

    add new map names to test new map patterns

--- examples/star_vii/map_choochoo.py
+++ examples/star_vii/map_choochoo.py
@@ -5,8 +5,9 @@ cols = 280
 
 #m = maps.get_map_realization("star_vii", "twochoochoo", rows=rows, columns=cols)
 #m = maps.get_map_realization("star_vii", "midnightexpress", rows=rows, columns=cols)
-m = maps.get_map_realization("star_vii", "spaceelevator", rows=rows, columns=cols)
+#m = maps.get_map_realization("star_vii", "spaceelevator", rows=rows, columns=cols)
 #m = maps.get_map_realization("star_vii", "faradaycage", rows=rows, columns=cols)
+m = maps.get_map_realization("star_vii", "housewithears", rows=rows, columns=cols)
 
 print(
     "http://localhost:8000/simulator/index.html"
