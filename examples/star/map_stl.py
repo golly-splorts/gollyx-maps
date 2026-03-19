41befa4c3abc0782321c786ad004688448481beb
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2026-03-18 18:49:51 -0700

    update examples

--- examples/star/map_stl.py
+++ examples/star/map_stl.py
@@ -3,10 +3,9 @@ from gollyx_maps import maps
 rows = 160
 cols = 240
 
-m = maps.get_map_realization("star", "combs", rows=rows, columns=cols)
+m = maps.get_map_realization("star", "newyork", rows=rows, columns=cols)
 print(
     "http://localhost:8000/simulator/index.html"
     + m["url"]
     + f"&rows={rows}&cols={cols}&cellSize=3"
 )
-
